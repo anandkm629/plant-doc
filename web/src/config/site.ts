8749eef179fac39bc8c6639573a16199ad8542a7
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-23 23:48:16 +0530

    initial commit

---
+++ web/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "PlantDoc",
+  url: "https://rudraksh.vercel.app",
+  ogImage: "https://rudraksh.vercel.app/og.jpg",
+  description:
+    "Your digital gateway to the botanical world. Unearth a wealth of insights, from species details to disease detection, effortlessly bridging the gap between technology and the beauty of nature.",
+  links: {
+    twitter: "https://twitter.com/anand_629",
+    github: "https://github.com/anandkm629",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
